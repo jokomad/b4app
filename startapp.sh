682f6ba24efc88816c7a9d4222111350a6339de1
jokomad <83660420+jokomad@users.noreply.github.com>
2024-05-13 21:09:31 +0200

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -3,8 +3,12 @@
 #exec tar -xf /1.tar.gz -C /tmp/1
 #exec chromium --renderer-process-limit=2  --origin-trial-disabled-features=WebGPU --disable-partial-raster --disable-mipmap-generation --render-process-limit=1 --disable-zero-copy --media-router=0 --disable-pings --no-default-browser-check --debug-devtools-frontend=0 --render-process-limit=1 --disable-zero-copy --disable-partial-raster --disable-mipmap-generation --memory-model=low --disable-features=GlobalMediaControls --disable-browser-side-navigation --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --no-sandbox --user-data-dir=/home/j/1 --disable-site-isolation-trials --enable-low-end-device-mode --renderer-process-limit=1
 
+#no-gui
+#exec chromium freebitco.in --headless=new --remote-allow-origins=* --user-data-dir=/config/1 --no-sandbox --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --disable-logging --disable-site-isolation-trials 
 
-exec chromium freebitco.in --headless=new --remote-allow-origins=* --user-data-dir=/config/1 --no-sandbox --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --disable-logging --disable-site-isolation-trials 
+#yes-gui
+exec chromium freebitco.in --user-data-dir=/config/1 --no-sandbox --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --disable-logging --disable-site-isolation-trials 
+
 
 #working without click tampemonkey icon --user-data-dir=/config/1 --no-sandbox --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --disable-logging --disable-site-isolation-trials
 
